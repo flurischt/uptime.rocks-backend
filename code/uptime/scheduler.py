b73e9a0fb060e3e25ef5882243cb6a775b239764
Flurin Rindisbacher <info@flurischt.ch>
2017-12-20 14:59:30 +0100

    implement scheduler

--- code/uptime/scheduler.py
+++ code/uptime/scheduler.py
@@ -1,39 +1,37 @@
 import json
 import os
+import time
 import boto3
+
+from boto3.dynamodb.conditions import Attr
 from uptime.logging import sentry_client, logger
 
 # dynamodb prototype
 status_table = boto3.resource('dynamodb').Table(os.getenv('TABLE_NAME'))
 
+lambdas = boto3.client('lambda')
+worker_name = os.getenv['WORKER_LAMBDA_NAME']
+
+UPDATE_INTERVAL_IN_SEC = 5 * 60
+
 @sentry_client.capture_exceptions
 def handler(event, context):
     logger.info('scheduler.handler got event {}'.format(event))
-    # # let's try out dynamodb
-    # from random import randint
-    # import time
-    # for _ in range(100):
-    #     id = str(randint(0, 10000))
-    #     status_table.put_item(
-    #         Item={
-    #             'id': id,
-    #             'last_check': int(time.time()),
-    #             'label': 'label nr. {}'.format(id),
-    #             'whatever': 'bli bla blu',
-    #         }
-    #     )
-
-    # async invoke the worker
-    client = boto3.client('lambda')
-    worker = os.environ['WORKER_LAMBDA_NAME']
-    logger.info('asynchronously invoking lambda: {}'.format(worker))
-    client.invoke(
-        FunctionName = worker,
-        InvocationType = 'Event',
-        Payload = json.dumps(
-            {
-                'reason': 'test'
-            }
-        ),
+    # get all services that have not been checked in the last UPDATE_INTERVAL_IN_SEC seconds
+    last_check_time = int(time.time()) - UPDATE_INTERVAL_IN_SEC
+    response = status_table.scan(
+        FilterExpression = Attr('last_check').lt(last_check_time)
     )
-    return 'Success?'
+    logger.info('Found {} services to be checked.'.format(response['Count']))
+    # and let the workers process them
+    for item in response['Items']:
+        logger.info('asynchronously invoking lambda: {} for id={}'.format(worker_name, item['id']))
+        lambdas.invoke(
+            FunctionName = worker_name,
+            InvocationType = 'Event',
+            Payload = json.dumps(item), # TODO: maybe only transfer the id and not the whole item?
+        )
+
+    return json.dumps({
+        'num_enqueued': response['Count']
+    })
--- code/uptime/worker.py
+++ code/uptime/worker.py
@@ -2,5 +2,6 @@ from uptime.logging import sentry_client, logger
 
 @sentry_client.capture_exceptions
 def handler(event, context):
-    logger.info('worker.handler got event {}'.format(event))
+    logger.info('worker.handler started for service-id: {}'.format(event['id']))
+    # TODO: check the service and update status table
     return 'Success'
