ddee06440423b253bd5bc4dc041fca21c84bddd4
Flurin Rindisbacher <info@flurischt.ch>
2017-12-20 14:05:44 +0100

    disable dynamodb testwrites

--- code/uptime/scheduler.py
+++ code/uptime/scheduler.py
@@ -9,19 +9,19 @@ status_table = boto3.resource('dynamodb').Table(os.getenv('TABLE_NAME'))
 @sentry_client.capture_exceptions
 def handler(event, context):
     logger.info('scheduler.handler got event {}'.format(event))
-    # let's try out dynamodb
-    from random import randint
-    import time
-    for _ in range(100):
-        id = str(randint(0, 10000))
-        status_table.put_item(
-            Item={
-                'id': id,
-                'last_check': int(time.time()),
-                'label': 'label nr. {}'.format(id),
-                'whatever': 'bli bla blu',
-            }
-        )
+    # # let's try out dynamodb
+    # from random import randint
+    # import time
+    # for _ in range(100):
+    #     id = str(randint(0, 10000))
+    #     status_table.put_item(
+    #         Item={
+    #             'id': id,
+    #             'last_check': int(time.time()),
+    #             'label': 'label nr. {}'.format(id),
+    #             'whatever': 'bli bla blu',
+    #         }
+    #     )
 
     # async invoke the worker
     client = boto3.client('lambda')
