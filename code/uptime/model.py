969e1263d286c09e2ebea09beacc8504e57bd0a7
Flurin Rindisbacher <info@flurischt.ch>
2017-12-21 15:30:40 +0100

    refactoring: move database code into its own module

---
+++ code/uptime/model.py
@@ -0,0 +1,69 @@
+import uuid
+import time
+from boto3.dynamodb.conditions import Attr
+
+SCHEMA = {
+    'id': '',
+    'schema_version': 1,
+    'label': '',
+    'url': '',
+    'last_check': 0,
+    'next_check': 0,
+    'check_interval': 0,
+}
+
+status_table = boto3.resource('dynamodb').Table(os.getenv('TABLE_NAME'))
+
+
+def _update_next_check(item):
+    """
+    helper function to update the next_check attribute.
+    """
+    item['next_check'] = item['last_check'] + item['check_interval']
+
+
+def put_item(**kwargs): 
+    """
+    create an item in DynamoDB using uptime.model.SCHEMA and return its id.
+
+    pass keyword-arguments to the function to set the attributes. default entries for `id` and `last_check` will be set if not given.
+    """
+    new_item = SCHEMA.copy()
+    for key, value in kwargs.items():
+        if key not in new_item.keys():
+            raise Exception('SCHEMA does not support attribute {}'.format(key))
+        new_item[key] = value
+    if 'id' not in kwargs.keys():
+        new_item['id'] = str(uuid.uuid4())
+    if 'last_check' not in kwargs.keys():
+        new_item['last_check'] = int(time.time())
+    _update_next_check(new_item) # TODO: business logic in here?
+    status_table.put_item(
+        Item=new_item
+    )
+    return new_item.id
+
+
+def scan_for_services_to_check():
+    """
+    yields items in DynamoDB with next_check < CURRENT_TIME
+    """
+    current_time = int(time.time())
+    response = status_table.scan(
+        ProjectionExpression='id',
+        FilterExpression = Attr('next_check').lte(current_time)
+    )
+    # handle pagination
+    while 'LastEvaluatedKey' in response.keys():
+        last_key = response['LastEvaluatedKey']
+        for item in response['Items']:
+            yield item
+        response = status_table.scan(
+            ProjectionExpression = 'id',
+            ExclusiveStartKey = last_key,
+            FilterExpression = Attr('next_check').lte(current_time)
+        )
+    # and yield the remaining elements
+    for item in response['Items']:
+        yield item
+    
--- code/uptime/scheduler.py
+++ code/uptime/scheduler.py
@@ -1,38 +1,25 @@
 import json
 import os
-import time
 import boto3
-
-from boto3.dynamodb.conditions import Attr
 from uptime.logging import sentry_client, logger
-
-# dynamodb prototype
-status_table = boto3.resource('dynamodb').Table(os.getenv('TABLE_NAME'))
+from uptime.model import scan_for_services_to_check
 
 lambdas = boto3.client('lambda')
 worker_name = os.getenv('WORKER_LAMBDA_NAME')
 
-UPDATE_INTERVAL_IN_SEC = 5 * 60
-
 @sentry_client.capture_exceptions
 def handler(event, context):
     logger.info('scheduler.handler got event {}'.format(event))
-    # get all services that have not been checked in the last UPDATE_INTERVAL_IN_SEC seconds
-    last_check_time = int(time.time()) - UPDATE_INTERVAL_IN_SEC
-    response = status_table.scan(
-        ProjectionExpression='id',
-        FilterExpression = Attr('last_check').lt(last_check_time)
-    )
-    logger.info('Found {} services to be checked.'.format(response['Count']))
     # and let the workers process them
-    for item in response['Items']:
-        logger.info('asynchronously invoking lambda: {} for id={}'.format(worker_name, item['id']))
+    count = 0
+    for service in scan_for_services_to_check():
+        logger.info('asynchronously invoking lambda: {} for id={}'.format(worker_name, service['id']))
         lambdas.invoke(
             FunctionName = worker_name,
             InvocationType = 'Event',
-            Payload = json.dumps(item),
+            Payload = json.dumps(service),
         )
-
+        count += 1
     return json.dumps({
-        'num_enqueued': response['Count']
+        'num_enqueued': count
     })
