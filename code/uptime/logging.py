d40fbb850b733d915b537813217c0d62643ce704
Flurin Rindisbacher <info@flurischt.ch>
2017-12-28 14:05:59 +0100

    switch sentry from LambdaClient back to Client
    Our Lambdas fail with https://github.com/getsentry/raven-python/issues/1169 when using captureMessage() or captureException(). This is not the case when using Client(). The whole HTTPTransport thingy was added according to https://github.com/getsentry/raven-python/issues/1063

--- code/uptime/logging.py
+++ code/uptime/logging.py
@@ -1,8 +1,9 @@
 import logging
 import local_secrets
-from raven.contrib.awslambda import LambdaClient
+from raven import Client
+from raven.transport.http import HTTPTransport
 
 # provide some defaults to the other modules
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-sentry_client = LambdaClient(local_secrets.SENTRY_DSN)
+sentry_client = Client(dsn=local_secrets.SENTRY_DSN, transport=HTTPTransport)
