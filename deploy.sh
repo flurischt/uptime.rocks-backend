86f5cb75d566b753c1000604b8d9109771f81902
Flurin Rindisbacher <info@flurischt.ch>
2017-12-29 16:05:57 +0100

    only package code.zip if thre are uncommited changes (commits should be deployed using CodeBuild)

--- deploy.sh
+++ deploy.sh
@@ -8,24 +8,37 @@
 # we'll use pylint and awscli in the virtualenv
 . ./venv/bin/activate
 
-# get requirements and put everything into code.zip
-echo packaging code.zip
-rm -rf code/requirements/*
-touch code/requirements/.gitkeep
-pip install -q --upgrade -r code/requirements.txt -t code/requirements/
+function package {
+        # get requirements and put everything into code.zip
+        echo packaging code.zip
+        rm -rf code/requirements/*
+        touch code/requirements/.gitkeep
+        pip install -q --upgrade -r code/requirements.txt -t code/requirements/
 
-PYTHONPATH=code/requirements/ pylint -E code/uptime
-if [ $? -ne 0 ]; then
-  echo "pylint reported errors! Not deploying..."
-  exit
+        PYTHONPATH=code/requirements/ pylint -E code/uptime
+        if [ $? -ne 0 ]; then
+          echo "pylint reported errors! Not deploying..."
+          exit
+        fi
+
+        # add our code and the requirements/ directory to the zip
+        cd code/ 
+        zip ../code.zip -qr ./uptime ./local_secrets.py
+        cd requirements/
+        zip ../../code.zip --grow -qr ./*
+        cd ../../
+}
+
+git status code/ | grep "nothing to commit, working directory clean" > /dev/null
+if [ $? -eq 0 ]; then
+    if [ -f code.zip ]; then
+        echo "no code-changes. redeploying existing code.zip"
+    else
+        package
+    fi
+else
+    package
 fi
-
-# add our code and the requirements/ directory to the zip
-cd code/ 
-zip ../code.zip -qr ./uptime ./local_secrets.py
-cd requirements/
-zip ../../code.zip --grow -qr ./*
-cd ../../
 
 # package
 aws cloudformation package \
